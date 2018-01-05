117e0814e8032f8d15b5a5f49851b80d8080c627
zelbar <zeljko.baranek@gmail.com>
2018-01-05 22:23:47 +0100

    Add dummy formally valid solution finder
    Puts all tests in different time spans on first machine.

---
+++ HMO/ScheduledTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMO
+{
+    public class ScheduledTest
+    {
+        public Test Test { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime => StartTime + Test.Duration;
+        public string Machine { get; set; }
+    }
+}
---
+++ HMO/Timetable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HMO
+{
+    public class Timetable
+    {
+        private readonly Test[] _tests;
+        private readonly string[] _machines;
+        private readonly Dictionary<string, int> _resources;
+        private List<ScheduledTest> _schedule;
+
+        public Timetable(Test[] tests, string[] machines, Dictionary<string, int> resources)
+        {
+            _tests = tests;
+            _machines = machines;
+            _resources = resources;
+
+            _schedule = new List<ScheduledTest>();
+        }
+
+        public IEnumerable<string> Calculate()
+        {
+
+            int globalTime = 0;
+
+            foreach (var test in _tests)
+            {
+                var st = new ScheduledTest()
+                {
+                    Test = test,
+                    StartTime = globalTime,
+                    Machine = test.MachinesItCanRunOn.Count() > 0 ?
+                        test.MachinesItCanRunOn.First() : _machines.First()
+                };
+                globalTime += test.Duration;
+                _schedule.Add(st);
+            }
+
+            return _schedule
+                .Select(x => "\'" + x.Test.Name + "\'," + x.StartTime + ",\'" + x.Machine + "\'.")
+                .ToList();
+        }
+
+        public int MaxDuration => _tests.Sum(x => x.Duration);
+        
+    }
+}
